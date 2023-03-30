3247eb397b599d07a71bfdc7a53a9d76d8884be0
VHall1 <victorhallpsn@hotmail.com>
2023-03-30 03:00:02 +0100

    feat: stub user-resolver

---
+++ backend/src/resolvers/user-resolver.ts
@@ -0,0 +1,13 @@
+import { Arg, Ctx, Query, Resolver } from "type-graphql";
+import { User } from "@generated/type-graphql";
+import { Context } from "../context";
+
+@Resolver(User)
+export class UserResolver {
+  @Query(() => User, { nullable: true })
+  show(@Ctx() ctx: Context, @Arg("id") id: User["id"]): Promise<User | null> {
+    return ctx.prisma.user.findUnique({
+      where: { id },
+    });
+  }
+}
